cb88f7d481f1568dcae5d70dc1d3db9f92e21be4
Federico Agustin Salinas <fedeasalinas@gmail.com>
2026-05-09 19:56:24 -0300

    se agrega la lógica en el archivo service

--- src/services/proyectoService.js
+++ src/services/proyectoService.js
@@ -1,1 +1,35 @@
-// en este archivo se realiza la lógica
+const proyectoService = (() => {
+
+    let proyectos = [
+        { id: 1, titulo: 'Capacitación de Docentes', categoria: 'web', estado: 'en curso' },
+        { id: 2, titulo: 'Biblioteca Digital', categoria: 'web', estado: 'completado' },
+        { id: 3, titulo: 'Olimpiadas académicas', categoria: 'mobile', estado: 'en curso' },
+        { id: 4, titulo: 'Orientación Vocacional', categoria: 'web', estado: 'completado' },
+        { id: 5, titulo: 'Tutoring Académico', categoria: 'mobile', estado: 'en curso' },
+    ];
+
+    const obtenerProyectos = () => [...proyectos];
+
+    const agregarProyecto = (proyecto) => {
+        const nuevoProyecto = { id: proyectos.length + 1, ...proyecto };
+        proyectos.push(nuevoProyecto);
+    }
+
+    const eliminarProyecto = (id) => {
+        proyectos = proyectos.filter(proyecto => proyecto.id !== id);
+    };
+
+    const buscarProyectos = (nombreProyecto) => {
+        const proyectoEncontrado = proyectos.filter(proyecto => proyecto.titulo.toLowerCase().includes(nombreProyecto.toLowerCase()));
+        return proyectoEncontrado;
+    }
+
+    return {
+        obtenerProyectos,
+        agregarProyecto,
+        eliminarProyecto,
+        buscarProyectos
+    };
+})()
+
+export default proyectoService;
