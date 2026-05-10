23a3634130b522fefadab9b413ecbe30b958e488
Joaquín nicolás Ibañez chicchi <ibanezjoaquin024@gmail.com>
2026-05-10 11:10:52 -0300

    se realizo la lista de proyectos

--- src/components/ListaProyecto.jsx
+++ src/components/ListaProyecto.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import proyectoService from "../services/proyectoService"
+import React, { useState } from 'react'
 
 const ListaProyecto = () => {
+    const [proyectos,setProyectos]=useState(proyectoService.obtenerProyectos())
     return (
-        <div>ListaProyecto</div>
-    )
+        <div>
+            <h2>Lista de Proyectos Educativos</h2>
+            {
+            proyectos.map(p=>(
+                <article>
+                    <h3>{p.titulo}</h3>
+                    <p>{p.categoria}</p>
+                    <p>{p.estado}</p>
+                </article>
+            )
+
+            )
+            }
+        </div>
+    );
 }
 
 export default ListaProyecto
