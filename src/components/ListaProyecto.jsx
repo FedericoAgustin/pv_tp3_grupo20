64a584a6b8a437f26e69eb167f7851e7aa67f510
nicoloop745 <206437489+Nicolop745@users.noreply.github.com>
2026-05-23 20:53:40 -0300

    se creo el componente FormularioProyecto de nicolop745

--- src/components/ListaProyecto.jsx
+++ src/components/ListaProyecto.jsx
@@ -1,30 +1,15 @@
 import proyectoService from "../services/proyectoService";
-import React, { useState } from "react";
+import  { useState } from "react";
 import ProyectoCard from "./ProyectoCard.jsx";
 import DetalleProyecto from "./DetalleProyecto.jsx";
 import { CambiarBusqueda } from "./CambiarBusqueda";
+import FormularioProyecto from "./FormularioProyecto.jsx";
 
 const ListaProyecto = ({ setMostrarFooter }) => {
   const [proyectos, setProyectos] = useState(proyectoService.obtenerProyectos(),);
   const [titulo, setTitulo] = useState("");
   const [proyectoseleccionado, setproyectoseleccionado] = useState(null);
-  const [form, setForm] = useState({
-    nombreProyecto: "",
-    categoriaProyecto: "",
-    estadoProyecto: "",
-    descripcionProyecto: "",
-    equipoProyecto: "",
-    rolProyecto: "",
-  });
   const [cambiarBusqueda, setCambiarBusqueda] = useState(false);
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setForm((prevForm) => ({
-      ...prevForm,
-      [name]: value,
-    }));
-  }
 
   const handleEliminar = (id) => {
     proyectoService.eliminarProyecto(id);
@@ -42,8 +27,7 @@ const ListaProyecto = ({ setMostrarFooter }) => {
     setProyectos(proyectoService.buscarProyectos(valor));
   };
 
-  const handleAgregar = (e) => {
-    e.preventDefault();
+  const handleAgregar = (form) => {
     const nuevoProyecto = {
       titulo: form.nombreProyecto,
       categoria: form.categoriaProyecto,
@@ -56,15 +40,6 @@ const ListaProyecto = ({ setMostrarFooter }) => {
 
     proyectoService.agregarProyecto(nuevoProyecto);
     setProyectos(proyectoService.obtenerProyectos());
-    // Limpiamos campos
-    setForm({
-      nombreProyecto: "",
-      categoriaProyecto: "",
-      estadoProyecto: "",
-      descripcionProyecto: "",
-      equipoProyecto: "",
-      rolProyecto: "",
-    });
   };
 
   const handleVerDetalle = (id) => {
@@ -96,23 +71,7 @@ const ListaProyecto = ({ setMostrarFooter }) => {
         </section>
         {/*FORMULARIO (Usando los estilos de aside-proyectos) */}
         <aside className="aside-proyectos">
-          <h3>Nuevo Proyecto</h3>
-          <form onSubmit={handleAgregar}>
-            <div className="form">
-              <input type="text" id="nombreProyecto" name="nombreProyecto" value={form.nombreProyecto} onChange={(e) => handleInputChange(e)} placeholder="Nombre del proyecto" required />
-              <input type="text" id="categoriaProyecto" name="categoriaProyecto" value={form.categoriaProyecto} onChange={(e) => handleInputChange(e)} placeholder="Categoría" required />
-              <input type="text" id="equipoProyecto" name="equipoProyecto" value={form.equipoProyecto} onChange={(e) => handleInputChange(e)} placeholder="Equipo a cargo" required />
-              <input type="text" id="rolProyecto" name="rolProyecto" value={form.rolProyecto} onChange={(e) => handleInputChange(e)} placeholder="Rol" required />
-              <select id="estadoProyecto" name="estadoProyecto" value={form.estadoProyecto} onChange={(e) => handleInputChange(e)} required>
-                <option value="">Selecciona un estado</option>
-                <option value="En curso">En curso</option>
-                <option value="Finalizado">Finalizado</option>
-                <option value="Pendiente">Pendiente</option>
-              </select>
-              <textarea id="descripcionProyecto" name="descripcionProyecto" value={form.descripcionProyecto} onChange={handleInputChange} placeholder="Descripción del proyecto" rows={4} required />
-              <button className="link" style={{ padding: "10px", borderRadius: "5px", marginTop: "10px", }} type="submit">Agregar Proyecto</button>
-            </div>
-          </form>
+         <FormularioProyecto handleAgregar={handleAgregar}/>
         </aside>
       </div>
     </main>
