281da374f4fdce5311c10f270b340023c39cf089
Federico Agustin Salinas <fedeasalinas@gmail.com>
2026-05-24 20:56:26 -0300

    Se llama al componente RegistroActividad dentro de ListraProyeto y se modifica la generación del id al agregar un proyecto

--- src/services/proyectoService.js
+++ src/services/proyectoService.js
@@ -5,7 +5,7 @@ import imagenOrientacion from './../static/imagenes/orientacion.webp';
 import imagenCursos from './../static/imagenes/cursos.webp';
 import imagenDefault from './../static/imagenes/default.webp';
 
-const descripcionArreglo=[
+const descripcionArreglo = [
     `Este proyecto tiene como objetivo fortalecer las competencias digitales de los docentes
     mediante la incorporación de herramientas tecnológicas en el aula. Se busca promover
     el uso de plataformas educativas, recursos interactivos y metodologías innovadoras
@@ -14,7 +14,7 @@ const descripcionArreglo=[
     para diseñar actividades digitales, evaluar el aprendizaje en entornos virtuales y
     fomentar la participación activa de los estudiantes. Además, se brindará acompañamiento
     continuo para asegurar la implementación efectiva de lo aprendido en contextos reales.`,
-     `La biblioteca digital es una alternativa diseñada para almacenar, organizar y facilitar 
+    `La biblioteca digital es una alternativa diseñada para almacenar, organizar y facilitar 
     el acceso a una amplia variedad de recursos informativos en formato electrónico.
     En este proyecto, ofrecemos acceder a libros en formato PDF, Drives con videos,
     Reporsitorios Github con proyectos guias.
@@ -32,7 +32,7 @@ const descripcionArreglo=[
     En este proyecto, ofrecemos distintas herramientas, como entrevistas, test y actividades de reflexión, se busca que cada 
     individuo pueda conocerse mejor y descubrir qué áreas se ajustan más a su perfil.
     En este proyecto, no solo se centra en elegir una carrera, sino también en comprender las oportunidades del entorno`,
-     `El tutoría académico es un proceso de acompañamiento educativo que tiene como objetivo apoyar a los estudiantes en su aprendizaje,
+    `El tutoría académico es un proceso de acompañamiento educativo que tiene como objetivo apoyar a los estudiantes en su aprendizaje,
      ayudándolos a mejorar su rendimiento, comprender mejor los contenidos y desarrollar hábitos de estudio efectivos.
      A través de la guía de un tutor, los estudiantes pueden resolver dudas, reforzar temas específicos y adquirir estrategias que faciliten su proceso de formación.`
 ];
@@ -41,17 +41,22 @@ const descripcionArreglo=[
 const proyectoService = (() => {
 
     let proyectos = [
-        { id: 1, titulo: 'Capacitación de Docentes', categoria: 'web', estado: 'En curso', imagen: imagenDocentes, equipo: 'Juan Pérez', rol: 'Director Principal',descripcion:descripcionArreglo[0]},
-        { id: 2, titulo: 'Biblioteca Digital', categoria: 'web', estado: 'Completado', imagen: imagenBiblioteca, equipo: 'Ana Martínez', rol: 'Coordinadora Secundario',descripcion:descripcionArreglo[1]},
-        { id: 3, titulo: 'Olimpiadas académicas', categoria: 'mobile', estado: 'En curso', imagen: imagenOlimpiadas, equipo: 'Sofía López', rol: 'Rectora',descripcion:descripcionArreglo[2]},
-        { id: 4, titulo: 'Orientación Vocacional', categoria: 'web', estado: 'Pendiente', imagen: imagenOrientacion, equipo: 'Elena Torres', rol: 'Administrativa', descripcion:descripcionArreglo[3]},
-        { id: 5, titulo: 'Tutoring Académico', categoria: 'mobile', estado: 'En curso', imagen: imagenCursos, equipo: 'Miguel Herrera', rol: 'Académico',descripcion:descripcionArreglo[4]},
+        { id: 1, titulo: 'Capacitación de Docentes', categoria: 'web', estado: 'En curso', imagen: imagenDocentes, equipo: 'Juan Pérez', rol: 'Director Principal', descripcion: descripcionArreglo[0] },
+        { id: 2, titulo: 'Biblioteca Digital', categoria: 'web', estado: 'Completado', imagen: imagenBiblioteca, equipo: 'Ana Martínez', rol: 'Coordinadora Secundario', descripcion: descripcionArreglo[1] },
+        { id: 3, titulo: 'Olimpiadas académicas', categoria: 'mobile', estado: 'En curso', imagen: imagenOlimpiadas, equipo: 'Sofía López', rol: 'Rectora', descripcion: descripcionArreglo[2] },
+        { id: 4, titulo: 'Orientación Vocacional', categoria: 'web', estado: 'Pendiente', imagen: imagenOrientacion, equipo: 'Elena Torres', rol: 'Administrativa', descripcion: descripcionArreglo[3] },
+        { id: 5, titulo: 'Tutoring Académico', categoria: 'mobile', estado: 'En curso', imagen: imagenCursos, equipo: 'Miguel Herrera', rol: 'Académico', descripcion: descripcionArreglo[4] },
     ];
 
     const obtenerProyectos = () => [...proyectos];
 
     const agregarProyecto = (proyecto) => {
-        const nuevoProyecto = { id: proyectos.length + 1, ...proyecto, imagen: imagenDefault };
+        const nuevoProyecto = {
+            id: Date.now(),
+            ...proyecto,
+            imagen: imagenDefault
+        };
+
         proyectos.push(nuevoProyecto);
     }
 
