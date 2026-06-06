e75860c5830640b9ae0589e0de64993de003b7cc
nehuelgonzalez <nehuelgonzalez24@gmail.com>
2026-06-06 18:12:15 -0300

    Arreglo error en header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,9 @@ import Logo from './Logo.jsx';
 const Header = (props) => {
     let { titulo } = props.element;
     return (
-        <header classname="d-flex align-items-center justify-content-center gap-2 py-3">
+        <header className="d-flex align-items-center justify-content-center gap-2 py-3">
             <Logo />
-            <h1 classname="mb-0">{titulo}</h1>
+            <h1 className="mb-0">{titulo}</h1>
         </header>
     )
 }
