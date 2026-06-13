6b8b370dccc0e0211c9f00d53a99223a18d8f875
Joaquín nicolás Ibañez chicchi <ibanezjoaquin024@gmail.com>
2026-06-13 20:29:47 -0300

    desarrollo de usuario contexto

--- src/context/UsuarioContext.jsx
+++ src/context/UsuarioContext.jsx
@@ -1,1 +1,25 @@
 // Aqui va el contexto del usuario, para compartir la informacion del usuario en toda la aplicación
+import { createContext, useState } from "react";
+
+export const UsuarioContext = createContext();
+
+export const UsuarioProvider=({ children }) => {
+    const [usuario, setUsuario]= useState(() => {
+        return{
+            nombre: "Lopez Aguila",
+            dni: "11111111",
+            rol: "Docente",
+            intitucion: "Universidad Nacional de Jujuy"
+        }
+
+
+    });
+    const actualizarPerfil = (datosActualizados) =>{
+        setUsuario(datosActualizados);
+    }
+    return(
+        <UsuarioContext.Provider value = {{usuario, actualizarPerfil}}> 
+            {children}
+        </UsuarioContext.Provider>
+    );
+}
